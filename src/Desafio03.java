45088984afd3ad9ab972103598020b282e1f45b4
Ayrton <devayrtonsbarreto@gmail.com>
2021-12-14 10:13:04 -0300

    Desafio 03 completo

---
+++ src/Desafio03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Desafio03 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+        int total = a + b;
+        System.out.println("X = " + total);
+    }
+}
